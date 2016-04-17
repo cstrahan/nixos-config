9aa8b85ffdc496aa61127640e97546587bd4fc81
Charles Strahan <charles@cstrahan.com>
2016-04-17 16:41:09 -0400

    update power management unit

--- power-tune.nix
+++ power-tune.nix
@@ -6,14 +6,11 @@
     wantedBy = [ "multi-user.target" ];
     script = ''
       ${pkgs.powertop}/bin/powertop --auto-tune
+      ${pkgs.iw}/bin/iw dev wlp3s0 set power_save on
+      for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+        echo powersave > $cpu
+      done
     '';
     serviceConfig.Type = "oneshot";
   };
 }
-
-# TOD:
-# ----
-# iw dev wlp3s0 set power_save on
-# for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
-#   echo powersave > $cpu
-# done
